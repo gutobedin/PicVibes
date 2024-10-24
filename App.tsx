7c249a904e9c745d6f979b89e8b4f64daad59439
Gustavo Bedin <gutoobedin@gmail.com>
2024-10-24 16:14:15 -0300

    Adicionando Icons

--- App.tsx
+++ App.tsx
@@ -1,25 +1,26 @@
 import React from 'react';
-import {SafeAreaView, View} from 'react-native';
+import {SafeAreaView} from 'react-native';
 
 import {Text} from './src/components/Text/Text';
-import {Button} from './src/components/Button/Button';
 import {ThemeProvider} from '@shopify/restyle';
 import {theme} from './src/theme/theme';
+
+import {Icon} from './src/components/Icon/Icon';
 import {Box} from './src/components/Box/Box';
 
 function App(): React.JSX.Element {
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView>
-        <View style={{paddingHorizontal: 20}}>
-          <Text italic preset="headingLarge">
-            PicVibes
-          </Text>
-          <Button loading mb="s20" title="Primary" />
-          <Button mb="s20" preset="outline" title="Outline" />
-          <Button mb="s20" disabled title="Outline" />
-          <Button mb="s20" loading title="Loading" />
-        </View>
+        <Text italic preset="headingLarge">
+          PicVibes
+        </Text>
+        <Box flexDirection="row" gap="s16">
+          <Icon name="eyeOff" color="carrotSecondary" size={40} />
+          <Icon name="eyeOn" color="error" size={40} />
+          <Icon name="chevronRight" size={40} />
+          <Icon name="bellOn" size={40} />
+        </Box>
       </SafeAreaView>
     </ThemeProvider>
   );
---
+++ src/components/Icon/Icon.tsx
@@ -0,0 +1,84 @@
+import React from 'react';
+import {EyeOffIcon} from '../../assets/icons/EyeOffIcon';
+import {EyeOnIcon} from '../../assets/icons/EyeOnIcon';
+import {ThemeColors} from '../../theme/theme';
+import {useAppTheme} from '../../hooks/useAppTheme';
+import {ArrowLeft} from '../../assets/icons/ArrowLeft';
+import {ArrowRight} from '../../assets/icons/ArrowRight';
+import {Bell} from '../../assets/icons/Bell';
+import {BellOn} from '../../assets/icons/BellOn';
+import {Bookmark} from '../../assets/icons/Bookmark';
+import {BookmarkFill} from '../../assets/icons/BookmarkFill';
+import {Camera} from '../../assets/icons/Camera';
+import {Chat} from '../../assets/icons/Chat';
+import {ChatOn} from '../../assets/icons/ChatOn';
+import {Check} from '../../assets/icons/Check';
+import {ChevronRight} from '../../assets/icons/ChevronRight';
+import {Comment} from '../../assets/icons/Comment';
+import {FlashOff} from '../../assets/icons/FlashOff';
+import {FlashOn} from '../../assets/icons/FlashOn';
+import {Heart} from '../../assets/icons/Heart';
+import {HeartFill} from '../../assets/icons/HeartFill';
+import {Home} from '../../assets/icons/Home';
+import {Message} from '../../assets/icons/Message';
+import {More} from '../../assets/icons/More';
+import {NewPost} from '../../assets/icons/NewPost';
+import {Profile} from '../../assets/icons/Profile';
+import {ProfileFill} from '../../assets/icons/ProfileFill';
+import {Search} from '../../assets/icons/Search';
+import {Send} from '../../assets/icons/Send';
+import {Trash} from '../../assets/icons/Trash';
+import {Settings} from '../../assets/icons/Settings';
+
+export interface IconBase {
+  size?: number;
+  color?: string;
+}
+
+interface Props {
+  name: IconName;
+  color?: ThemeColors;
+  size?: number;
+}
+
+export function Icon({name, color = 'backgroundContrast', size = 20}: Props) {
+  const {colors} = useAppTheme();
+  const SVGIcon = iconRegistry[name];
+
+  return <SVGIcon color={colors[color]} size={size} />;
+}
+
+const iconRegistry = {
+  eyeOff: EyeOffIcon,
+  eyeOn: EyeOnIcon,
+  arrowLeft: ArrowLeft,
+  arrowRight: ArrowRight,
+  bell: Bell,
+  bellOn: BellOn,
+  bookmark: Bookmark,
+  bookmarkFill: BookmarkFill,
+  camera: Camera,
+  chat: Chat,
+  chatOn: ChatOn,
+  check: Check,
+  chevronRight: ChevronRight,
+  comment: Comment,
+  flashOff: FlashOff,
+  flashOn: FlashOn,
+  heart: Heart,
+  heartFill: HeartFill,
+  home: Home,
+  message: Message,
+  more: More,
+  newPost: NewPost,
+  profile: Profile,
+  profileFill: ProfileFill,
+  search: Search,
+  send: Send,
+  settings: Settings,
+  trash: Trash,
+};
+
+type IconType = typeof iconRegistry;
+
+type IconName = keyof IconType;
